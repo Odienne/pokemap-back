3c51a63a1ff5d9e0fd2f7f1284761c560b0f2a65
Adam <odienne.adam@gmail.com>
2021-12-16 09:12:20 +0100

    wip map

--- pokefrance/src/App.js
+++ pokefrance/src/App.js
@@ -1,7 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import 'leaflet/dist/leaflet.css';
 import React, { useState, useEffect } from 'react';
 import {getData} from "./services/api";
+
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+
 
 function App() {
   const [data, setData] = useState();
@@ -14,20 +18,14 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MapContainer center={[42, 6]} zoom={6} scrollWheelZoom={false}>
+        <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[44, 6]}>
+        </Marker>
+      </MapContainer>
     </div>
   );
 }
