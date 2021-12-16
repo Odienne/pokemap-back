ea7fbe7bbd8d3d7d35c001072b6ade1c3939eb62
Adam <odienne.adam@gmail.com>
2021-12-16 09:26:39 +0100

    fix default icon not being loaded correctly

--- pokefrance/src/App.js
+++ pokefrance/src/App.js
@@ -4,7 +4,17 @@ import React, { useState, useEffect } from 'react';
 import {getData} from "./services/api";
 
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import 'leaflet/dist/leaflet.css';
 
+import L from 'leaflet';
+import icon from 'leaflet/dist/images/marker-icon.png';
+import iconShadow from 'leaflet/dist/images/marker-shadow.png';
+let DefaultIcon = L.icon({
+  iconUrl: icon,
+  shadowUrl: iconShadow
+});
+
+L.Marker.prototype.options.icon = DefaultIcon;
 
 // jshint ignore:start
 function App() {
